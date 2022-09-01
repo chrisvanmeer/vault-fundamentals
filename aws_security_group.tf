2cb8c06ef3edf9a2a0812cf87eef67b28ab526f3
Chris van Meer <chris@van-meer.net>
2022-09-01 12:41:29 +0200

    Added egress rule.

--- aws_security_group.tf
+++ aws_security_group.tf
@@ -20,4 +20,12 @@ resource "aws_security_group" "vault_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 }
