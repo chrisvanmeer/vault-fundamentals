f1025d0dfa3e0942b24eeeefcc5c616bcb876624
Chris van Meer <chris@van-meer.net>
2022-09-01 11:20:05 +0200

    Fix

--- aws_security_group.tf
+++ aws_security_group.tf
@@ -3,17 +3,21 @@ resource "aws_security_group" "vault_sg" {
   description = "Allow Vault traffic"
 
   ingress {
-    description = "Vault API and cluster traffic"
-    from_port   = 8200
-    to_port     = 8201
-    protocol    = "tcp"
+    description      = "Vault API and cluster traffic"
+    from_port        = 8200
+    to_port          = 8201
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
-    description = "SSH traffic"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    description      = "SSH traffic"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
 }
