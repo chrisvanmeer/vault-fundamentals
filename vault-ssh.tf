61c06e1d087583a57e16cf3aeef1c30b336bec18
Chris van Meer <chris@van-meer.net>
2022-11-29 22:29:00 +0100

    Added SSH config generator.

---
+++ vault-ssh.tf
@@ -0,0 +1,14 @@
+resource "local_file" "tmux" {
+  content = templatefile("vault-ssh-config.tftpl",
+    {
+      vnode01  = aws_instance.vault_cluster[0].public_dns
+      vnode02  = aws_instance.vault_cluster[1].public_dns
+      vnode03  = aws_instance.vault_cluster[2].public_dns
+      vrepl01  = aws_instance.vault_replication[0].public_dns
+      vrepl02  = aws_instance.vault_replication[1].public_dns
+      vrepl03  = aws_instance.vault_replication[2].public_dns
+      identity = var.ssh_private_key_location
+    }
+  )
+  filename = "./ssh.config"
+}
